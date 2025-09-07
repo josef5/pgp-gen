801825aa7a8f1eb3a877960d3d7c1c38cd91b24b
Jose Espejo <joseespejo@yahoo.com>
2025-09-07 17:24:38 +0100

    refactor: replace @import for Space Mono with root route links export

--- app/root.tsx
+++ app/root.tsx
@@ -6,7 +6,6 @@ import {
   Scripts,
   ScrollRestoration,
 } from "react-router";
-
 import type { Route } from "./+types/root";
 import "./app.css";
 import NavBar from "./components/nav-bar";
@@ -20,7 +19,7 @@ export const links: Route.LinksFunction = () => [
   },
   {
     rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+    href: "https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap",
   },
 ];
 
