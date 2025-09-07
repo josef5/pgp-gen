67a7c71b36c14e6a4c52a23411dfab8c0cc0dc08
Jose Espejo <joseespejo@yahoo.com>
2025-09-07 14:13:23 +0100

    refactor: standardize font size in NavBar component

--- app/components/nav-bar.tsx
+++ app/components/nav-bar.tsx
@@ -9,19 +9,19 @@ function NavBar() {
     <nav className="flex bg-neutral-200">
       <NavLink
         to="/"
-        className={`flex h-12 w-56 items-center justify-center ${location.pathname === "/" ? "bg-neutral-100" : ""} text-[16px]`}
+        className={`flex h-12 w-56 items-center justify-center ${location.pathname === "/" ? "bg-neutral-100" : ""} text-base`}
       >
         Generate
       </NavLink>
       <NavLink
         to="/encrypt"
-        className={`flex h-12 w-56 items-center justify-center ${location.pathname === "/encrypt" ? "bg-neutral-100" : ""} text-[16px]`}
+        className={`flex h-12 w-56 items-center justify-center ${location.pathname === "/encrypt" ? "bg-neutral-100" : ""} text-base`}
       >
         Encrypt
       </NavLink>
       <NavLink
         to="/decrypt"
-        className={`flex h-12 w-56 items-center justify-center ${location.pathname === "/decrypt" ? "bg-neutral-100" : ""} text-[16px]`}
+        className={`flex h-12 w-56 items-center justify-center ${location.pathname === "/decrypt" ? "bg-neutral-100" : ""} text-base`}
       >
         Decrypt
       </NavLink>
