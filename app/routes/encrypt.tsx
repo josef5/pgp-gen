65d14ebe5baa3354644c47265bb0c02366245606
Jose Espejo <joseespejo@yahoo.com>
2025-09-07 21:54:06 +0100

    style: update textarea border radius and button background color for consistency

--- app/routes/encrypt.tsx
+++ app/routes/encrypt.tsx
@@ -14,7 +14,7 @@ function Encrypt() {
       </div>
       <textarea
         id="message"
-        className="focus:border-brand focus:ring-brand bg-background-secondary col-span-3 col-start-2 h-48 rounded border border-none px-5 py-2 text-base focus:ring-1 focus:outline-none"
+        className="focus:border-brand focus:ring-brand bg-background-secondary col-span-3 col-start-2 h-48 rounded-none border border-none px-5 py-2 text-base focus:ring-1 focus:outline-none"
         placeholder="Paste here"
       />
       <div className="col-start-1 h-12"></div>
@@ -25,11 +25,11 @@ function Encrypt() {
       </div>
       <textarea
         id="public-key"
-        className="focus:border-brand focus:ring-brand bg-background-secondary col-span-3 col-start-2 h-48 rounded border border-none px-5 py-2 text-base focus:ring-1 focus:outline-none"
+        className="focus:border-brand focus:ring-brand bg-background-secondary col-span-3 col-start-2 h-48 rounded-none border border-none px-5 py-2 text-base focus:ring-1 focus:outline-none"
         placeholder="Paste here"
       />
       <div className="col-span-2 col-start-1 h-12"></div>
-      <button className="border-foreground-secondary text-foreground-secondary active:bg-foreground active:text-background hover:border-hover col-span-3 col-start-2 h-10 w-full cursor-pointer border-1">
+      <button className="border-foreground-secondary text-foreground-secondary active:bg-background-secondary hover:border-hover col-span-3 col-start-2 h-10 w-full cursor-pointer border-1 active:border-none">
         Encrypt
       </button>
     </section>
