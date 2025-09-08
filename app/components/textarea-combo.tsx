bf94e24596deae668ba30cae495bc589b2b7ae26
Jose Espejo <joseespejo@yahoo.com>
2025-09-08 22:24:04 +0100

    feat: add TextareaCombo component for enhanced text input

---
+++ app/components/textarea-combo.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+function TextareaCombo({
+  label,
+  id,
+  placeholder,
+  ...props
+}: {
+  label: string;
+  id: string;
+  placeholder?: string;
+  [key: string]: any;
+}) {
+  return (
+    <>
+      <label
+        htmlFor={id}
+        className="text-brand col-span-3 col-start-1 mb-2 flex justify-start text-right text-xl md:col-span-1 md:ml-8"
+      >
+        {label}
+      </label>
+      <textarea
+        id={id}
+        className="bg-background-secondary col-span-3 col-start-1 box-border h-48 rounded-none border-none px-5 py-2 text-base focus:border-2 focus:bg-neutral-50 focus:outline-none md:col-start-2"
+        placeholder={placeholder}
+        {...props}
+      />
+    </>
+  );
+}
+
+export default TextareaCombo;
