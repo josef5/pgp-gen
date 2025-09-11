de9b39684c965d95aefa466dff5e21f0319ea4ba
Jose Espejo <joseespejo@yahoo.com>
2025-09-11 17:01:32 +0100

    refactor: simplify Textarea component by removing label and placeholder props

--- app/components/textarea.tsx
+++ app/components/textarea.tsx
@@ -1,35 +1,20 @@
 import { cn } from "@/lib/utils";
 
 function Textarea({
-  label,
-  id,
-  placeholder,
   className,
   ...props
 }: {
-  label: string;
-  id: string;
-  placeholder?: string;
+  className?: string;
   [key: string]: any;
 }) {
   return (
-    <>
-      <label
-        htmlFor={id}
-        className="text-brand col-span-3 col-start-1 mb-2 flex justify-start text-right text-xl md:col-span-1 md:ml-8"
-      >
-        {label}
-      </label>
-      <textarea
-        id={id}
-        className={cn(
-          "bg-background-secondary focus:bg-background-tertiary col-span-3 col-start-1 box-border h-48 rounded-none border-none px-5 py-2 text-base focus:border-2 focus:outline-none md:col-start-2",
-          className,
-        )}
-        placeholder={placeholder}
-        {...props}
-      />
-    </>
+    <textarea
+      className={cn(
+        "bg-background-secondary focus:bg-background-tertiary box-border h-48 rounded-none border-none px-5 py-2 text-base focus:border-2 focus:outline-none",
+        className,
+      )}
+      {...props}
+    />
   );
 }
 
