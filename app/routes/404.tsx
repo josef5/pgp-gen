a454857ba3ed72f64ccd7b2e218e18e22be02c52
Jose Espejo <joseespejo@yahoo.com>
2025-09-15 22:31:59 +0100

    feat: enhance NotFound component layout with structured HTML and styling

--- app/routes/404.tsx
+++ app/routes/404.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 function NotFound() {
-  return <div>404 - Not Found</div>;
+  return (
+    <section>
+      <div className="mr-4 ml-4 grid max-w-[896px] grid-cols-3 md:ml-0 md:grid-cols-4">
+        <div className="col-span-full col-start-1 h-24"></div>
+        <p className="col-start-1 ml-4">404 - Not Found</p>
+      </div>
+    </section>
+  );
 }
 
 export default NotFound;
