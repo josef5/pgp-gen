14ad5160b9dccc0a6f1f558e1f4c28a9b6a44fc3
Jose Espejo <joseespejo@yahoo.com>
2025-09-16 14:20:04 +0100

    chore: add gh-pages dependency and update settings for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: "/pgp-gen/",
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
