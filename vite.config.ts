033002a257a8c7bc5b50884c85d42f885f8361bc
Jose Espejo <joseespejo@yahoo.com>
2025-09-19 21:54:41 +0100

    chore: comment out base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base: "/pgp-gen/",
+  // base: "/pgp-gen",
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
