bdabbcfe7734f3893154862dda218073dab60fae
Jose Espejo <joseespejo@yahoo.com>
2025-09-21 23:21:37 +0100

    fix: uncomment base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: "/pgp-gen",
+  base: "/pgp-gen",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
